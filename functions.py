3358c1aa1f18a23f6b94bf5d3da6df5a2396645d
RogueNPC <jordanbtansay@gmail.com>
2020-09-07 09:22:01 -0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,32 @@
+#Q1
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+#Q3
+result = area(2, 7)
+print(result)
+result = area(13, 3)
+print(result)
+
+#Q4
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+#Q5
+difference = subtract(42, 24)
+print(difference)
+difference = subtract(99, 99)
+print(difference)
+
+#Q7
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+sum = add(23, 54)
+print(sum)
+sum = add (39, 49)
+print(sum)
